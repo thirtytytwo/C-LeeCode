00436255ac55e5589e7607a1401f718470efe0f8
Thirtytwo <3267207337@qq.com>
2021-09-30 08:25:37 +0800

    9.30

--- C++LeeCode.cpp
+++ C++LeeCode.cpp
@@ -678,6 +678,15 @@ public:
 		}
 		return ans;
 	}
+	//9.30 leecode223
+	int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+		int area = (ax2 - ax1) * (ay2 - ay1) + (bx2 - bx1) * (by2 - by1);
+
+		int overlapLong = max(0, (min(ax2, bx2) - max(ax1, bx1)));
+		int overlapWidth = max(0, (min(by2, ay2) - max(ay1, by1)));
+
+		return area - (overlapLong * overlapWidth);
+	}
 };
 
 int main() {
