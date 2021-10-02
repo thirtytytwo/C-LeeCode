a9cbc0384cb6e28e007719b5d5c9fce6e76dccd8
Thirtytwo <3267207337@qq.com>
2021-10-02 22:43:24 +0800

    10.2

--- C++LeeCode.cpp
+++ C++LeeCode.cpp
@@ -687,6 +687,25 @@ public:
 
 		return area - (overlapLong * overlapWidth);
 	}
+	//10.2 leecode405
+	string hex = "0123456789abcdef";
+	string toHex(int num) {
+		if (num == 0) {
+			return "0";
+		}
+
+		long long n = num < 0 ? (long long)num + (1ll << 32) : num;
+
+		string s;
+		while (n) {
+			int mod = n % 16;
+			n /= 16;
+			s += hex[mod];
+		}
+
+		reverse(s.begin(), s.end());
+		return s;
+	}
 };
 
 int main() {
